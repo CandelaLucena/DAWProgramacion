f82e944004fe878e1526ccde4e369b4ae87f1ff2
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-09-27 09:07:20 +0200

    Tres primeros ejemplos del Tema 1 acabados

---
+++ Tema1/Ejercicio2.java
@@ -0,0 +1,13 @@
+
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		double squareArea = 5;
+		double squareArea = 5;
+		double resultado = squareArea*squareArea;
+		System.out.println(resultado);
+	}
+
+}
---
+++ Tema1/HolaMundoJava.java
@@ -0,0 +1,8 @@
+
+public class HolaMundoJava {
+
+	public static void main(String[] args) {		
+		System.out.println("Hola Mundo");
+	}
+
+}
