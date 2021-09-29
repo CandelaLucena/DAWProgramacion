da383c4a86f6bbd4a99a9e4b2a83f29e3a6bcd11
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-09-29 10:06:11 +0200

    Ejercicio 4

---
+++ Ejercicio4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ejercicio4 {
+    public static void main (String args []) {
+        float side;
+        float squareArea; 
+        
+        System.out.println("Inserte un lado:");
+
+        Scanner inputValue; 
+        inputValue = new Scanner(System.in);
+        side = inputValue.nextFloat();
+
+        squareArea = side * side;
+        System.out.println(squareArea);
+
+    }
+}
