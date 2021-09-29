8ca427df4bca19fff53751ce18d69e7c21c3c404
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-09-29 10:34:06 +0200

    Ejercicio 4

--- Tema1/Ejercicio4.java
+++ Tema1/Ejercicio4.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 public class Ejercicio4 {
-    public static void main (String args []) {
+    public static void main (String args[]) {
         float side;
         float squareArea; 
         
         System.out.println("Inserte un lado:");
 
         Scanner inputValue; 
+
         inputValue = new Scanner(System.in);
+
         side = inputValue.nextFloat();
 
         squareArea = side * side;
         System.out.println(squareArea);
-
+        inputValue.close();
     }
+    
 }
