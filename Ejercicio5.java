1d6953795b5eed3fe084f2fc0358b280ae2ced52
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-09-29 12:30:09 +0200

    ejer 5

---
+++ Ejercicio5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+import org.graalvm.compiler.lir.gen.DiagnosticLIRGeneratorTool.ZapRegistersAfterInstruction;
+public class Ejercicio5 {
+    public static void main (String args[]) {
+
+int = x
+int = y
+int = z 
+
+
+        
+}
