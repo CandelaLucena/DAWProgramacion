1abffe040e34c4cceb85821dab874b35dc761e0a
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-09-29 09:31:14 +0200

    Ejercicio 2 y 3

--- Tema1/Ejercicio2.java
+++ Tema1/Ejercicio2.java
@@ -2,12 +2,17 @@
 
 public class Ejercicio2 {
 
-	public static void main(String[] args) {
-		
-		double squareArea = 5;
-		double squareArea = 5;
-		double resultado = squareArea*squareArea;
-		System.out.println(resultado);
-	}
+    public static void main(String[] args) {
 
-}
+        int dividendo = 20;
+
+        int divisor = 6;
+
+        int sumarAlFinal = 3;
+
+        int resultado = (dividendo/divisor) + sumarAlFinal;
+
+		System.out.println(resultado);
+
+    }
+} 
---
+++ Tema1/Ejercicio3.java
@@ -0,0 +1,11 @@
+
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		double squareArea = 5;
+		System.out.println(squareArea*squareArea);
+	}
+
+}
