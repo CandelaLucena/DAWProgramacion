de7707d7c1d1556143095ada49c145ab82fe5b95
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-09-29 23:19:08 +0200

    ejercicioos pa copiar

---
+++ Multiplicacion.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;	
+public class Multiplicacion 
+{
+
+	public static void main(String[] args) 
+	{	
+		Scanner sc = new Scanner(System.in);
+		int x, y, z;
+		System.out.println("Escriba dos numeros");
+
+			x = sc.nextInt ();
+			y = sc.nextInt ();
+			z = x*y;
+
+			if (z>=10)
+				System.out.println(z +", es mayor que 10");
+			else
+				System.out.println(z +", no es mayor que 10");
+	}
+
+	
+}
+
---
+++ Suma.java
@@ -0,0 +1,7 @@
+public class Suma {
+
+	public static void main(String[] args) {		
+		System.out.println("Hola Mundo");
+	}
+
+}
