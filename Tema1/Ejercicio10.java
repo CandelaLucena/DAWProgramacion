c08fb4ab75ade9e77d60a58c741011642999340a
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-10-01 10:35:02 +0200

    .

---
+++ Tema1/Ejercicio10.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        
+        System.out.println("Introduzca un numero");
+
+        x = sc.nextInt();
+            if (x >= 0) {
+                System.out.println("Positivo");
+                }else{
+                    System.out.println("Negativo");
+                }
+                sc.close();
+            }
+
+        
+}
