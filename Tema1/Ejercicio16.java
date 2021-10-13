3732eca2ba81dbd684fd7df8ef03d200d83bcef9
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-10-13 10:32:48 +0200

    Ejercicio 16

--- Tema1/Ejercicio16.java
+++ Tema1/Ejercicio16.java
@@ -2,14 +2,32 @@ import java.util.Scanner;
 public class Ejercicio16 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int operacion, x, y;
+        double x, y;
+        String operacion;
 
         System.out.println("Introduzca x e y");
         x = scanner.nextInt();
         y = scanner.nextInt();
-        System.out.println("Introduzca el simbolo de la operación");
-        operacion = scanner.nextInt();
-        scanner.close();
 
+        System.out.println("Introduzca el simbolo de la operación");
+        operacion = scanner.next();
+        scanner.close();
+        
+            switch (operacion){
+            case "+":
+                System.out.println(x + y);
+                break;
+            case "-":
+                System.out.println(x - y);
+                break;
+            case "/":
+                System.out.println(x / y);
+                break;
+            case "*":
+                System.out.println(x * y);
+                break;
+            default:
+                break;
+        }
     }
 }
