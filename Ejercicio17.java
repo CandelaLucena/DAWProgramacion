c5025996fd7fe7ac7190634924e426a0c7c95302
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-10-13 10:43:33 +0200

    Ejercicio 17

--- Ejercicio17.java
+++ Ejercicio17.java
@@ -1,3 +1,40 @@
+import java.util.Scanner;
 public class Ejercicio17 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int mes;
+
+        System.out.println("Introduzca el mes");
+        mes = scanner.nextInt();
+        scanner.close();
+
+        switch (mes){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+        case 11:
+            break;
+        case 12:
+            break;
+        default:
+            break;
+        }
+    }
 }
