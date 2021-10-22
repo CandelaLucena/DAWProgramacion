c6373da3c237fdae2f5e826a887c639c4995fb02
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-10-22 09:55:35 +0200

    Ejercicios 24, 24, 25

---
+++ Tema1/Ejercicio24.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Ejercicio24{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int notas, excelentes, contador;
+        double notamedia;
+        excelentes = 0;
+        notas = 0;
+        notamedia = 0;
+        contador = 0;
+
+        while (notas != -1){
+            System.out.println("Escribe las notas");
+            notas = scanner.nextInt();
+            if (notas > -1){
+                notamedia = notamedia + notas;
+                contador ++;
+            if (notas == 10){
+                excelentes = excelentes + 1;
+            }  
+            } 
+        }
+        
+        if (contador > 0){
+            System.out.println("Hay "+excelentes+" excelentes y la nota media es: "+notamedia/contador);
+        }else{
+            System.out.println("Hay 0 excelentes y la nota media es: "+0);
+        }
+        scanner.close();
+    }
+}
