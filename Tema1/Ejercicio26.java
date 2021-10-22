e5b66bef71b646ed0426b763f24f7cbf68d885b5
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-10-22 10:36:37 +0200

    Ejercicio 26

---
+++ Tema1/Ejercicio26.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ejercicio26{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        System.out.println("Escriba un numero para mostrar su tabla de multiplicar");
+        numero = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println(numero +" x "+ i +" = "+ numero*i);
+        }
+        scanner.close();
+    }
+}
