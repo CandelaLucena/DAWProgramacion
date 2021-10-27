dbdd395acb8af13773f8c0315e4e54e00ac9f064
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-10-27 10:33:30 +0200

    mates

---
+++ Tema2/MatesPruebas.java
@@ -0,0 +1,9 @@
+package Tema2;
+public class MatesPruebas{
+    public static void main(String [] args) {
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    }
+}
