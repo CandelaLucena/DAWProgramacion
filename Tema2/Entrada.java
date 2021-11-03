9581bc16e8963b3db95a3e82ab0b10e33f31b8ec
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-11-03 08:42:43 +0100

    Ejercicio Entrada

--- Tema2/Entrada.java
+++ Tema2/Entrada.java
@@ -6,17 +6,29 @@ public class Entrada {
     public static int getInteger(){
         Scanner scanner = new Scanner (System.in);
         boolean hayEntero = scanner.hasNextInt();
+        hayEntero = false;
         int numero;
         numero = 0;
+        String mensaje = "Escriba un entero";
+        System.out.println(mensaje);
+
         while (!hayEntero) {         
+            if(scanner.hasNextLine() && scanner.hasNextInt()){
+                numero = scanner.nextInt();
+                hayEntero = true;
+            }else{
+            System.out.println(mensaje);
+            scanner.close();
             scanner.nextLine();
+            }
         }
-        scanner.close();
         return numero;
     }
+}
+    
 
     // public static double getDouble(){
     //     hasNextDouble();
     //     return;
     // }
-}
+
