704a0fb6fae76acbbc56f143d8e8bff1876894de
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-11-05 09:33:39 +0100

    E04 terminado

---
+++ Tema2/Control02/E04.java
@@ -0,0 +1,15 @@
+package Tema2.Control02;
+
+import java.util.Scanner;
+
+public class E04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        double longitud = 0, pulgada = 2.54;
+        System.out.println("Ingrese longitud: ");
+        longitud = scanner.nextDouble();
+        scanner.close();
+        
+        System.out.println(longitud + "cm = "+(longitud / pulgada)+"in");
+    }
+}
