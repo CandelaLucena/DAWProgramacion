56f0e9e02f23f10247c8536b985eb2cc45411e9c
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-11-05 08:40:06 +0100

    Ejercicio29

--- Tema1/Ejercicio29.java
+++ Tema1/Ejercicio29.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Ejercicio29 {
     public static void main(String[] args) {
-        
+        int numero = 0;
+        int contador = 0;
+        System.out.println("Escriba un numero.");
+        Scanner scanner = new Scanner(System.in);
+        numero = scanner.nextInt();
+
+        for (int i = 1; i < numero; i++) {
+            if (numero % i == 0){
+                contador++;
+            }
+        }
+        if (contador == 2){
+            System.out.println("El numero es primo");
+        }else{
+            System.out.println("El numero no es primo");
+        }
+        scanner.close();
     }
 }
+
