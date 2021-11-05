313732289ef63f84b25b5d8b091499137a702f53
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-11-05 10:42:27 +0100

    E09 medio terminado, E10 sin terminar

---
+++ Tema2/Control02/E09.java
@@ -0,0 +1,20 @@
+package Tema2.Control02;
+
+import java.util.Scanner;
+
+public class E09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        double primeraNota = 0, segundaNota = 0, terceraNota = 0, notaLaboratorio = 0;
+        System.out.println("Ingrese nota certamen 1: ");
+        primeraNota = scanner.nextDouble();
+        System.out.println("Ingrese nota certamen 2: ");
+        segundaNota = scanner.nextDouble();
+        System.out.println("Ingrese nota laboratorio: ");
+        notaLaboratorio = scanner.nextDouble();
+
+        terceraNota = (60 - (notaLaboratorio * 0.3 * 3)) / (0.7 * primeraNota * segundaNota);
+
+        System.out.println("Necesita nota "+terceraNota+" en el certamen 3");
+    }
+}
