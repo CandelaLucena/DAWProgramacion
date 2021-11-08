3cedffdcd011d1bfff1675cd925ff6c5ab3ffa8f
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-11-08 08:10:56 +0100

    E07 terminado, E10 ni idea

--- Tema2/Control02/E07.java
+++ Tema2/Control02/E07.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class E07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-        int numeroHorasActuales = 0, numeroHorasAdelantadas = 0;
+        int numeroHorasActuales = 0, numeroHorasAdelantadas = 0, horas = 0;
         System.out.println("Ingrese hora actual: ");
         numeroHorasActuales = scanner.nextInt();
         System.out.println("Ingrese cantidad de horas: ");
         numeroHorasAdelantadas = scanner.nextInt();
+
+        horas = (numeroHorasActuales + numeroHorasAdelantadas) % 24;
+        System.out.println("En "+numeroHorasAdelantadas+" horas, el reloj marcará las "+horas);
         scanner.close();
         
     }
---
+++ Tema2/Control02/E10.java
@@ -0,0 +1,7 @@
+package Tema2.Control02;
+
+public class E10 {
+    public static void main(String[] args) {
+        
+    }
+}
