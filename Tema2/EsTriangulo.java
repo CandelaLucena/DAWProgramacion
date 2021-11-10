3617ea4ce3517701751e70e50100d7317d79e876
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-11-10 09:20:03 +0100

    esTriangulo terminado

---
+++ Tema2/EsTriangulo.java
@@ -0,0 +1,20 @@
+package Tema2;
+
+public class EsTriangulo {
+    
+    public static boolean esTriangulo(int palo1, int palo2, int palo3){
+        boolean resultado = true;
+
+        if (palo1+palo2 < palo3) {
+            resultado = false;
+        }
+        if (palo2+palo3 < palo1) {
+            resultado = false;
+        }
+        if (palo1+palo3 < palo2) {
+            resultado = false;
+        }
+
+        return resultado;
+    }
+}
