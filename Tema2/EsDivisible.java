b1d4d8ab7e71fb000d27f49cc91ade942e5c6a26
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-11-10 09:07:20 +0100

    esDivisible terminado

---
+++ Tema2/EsDivisible.java
@@ -0,0 +1,14 @@
+package Tema2;
+
+public class EsDivisible {
+    
+    public static boolean esDivisible(int n, int m){
+        boolean resultado = false;
+
+        resultado = ((n%m) == 0);
+        
+        return resultado;
+    }
+}
+    
+
---
+++ Tema2/EsDivisiblePrueba.java
@@ -0,0 +1,14 @@
+package Tema2;
+
+public class EsDivisiblePrueba {
+    public static void main(String[] args) {
+        boolean prueba1 = EsDivisible.esDivisible (4,2);
+        boolean prueba2 = EsDivisible.esDivisible (5,2);
+        boolean prueba3 = EsDivisible.esDivisible (5,5);
+        boolean prueba4 = EsDivisible.esDivisible (8,9);
+        System.out.println(prueba1);
+        System.out.println(prueba2);
+        System.out.println(prueba3);
+        System.out.println(prueba4);
+    }
+}
