58fa8f5cd7d28bd000afadaadb5a46f174ce9741
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-11-10 09:35:49 +0100

    Potencia terminado

---
+++ Tema2/Potencia.java
@@ -0,0 +1,8 @@
+package Tema2;
+
+public class Potencia {
+    public static double potencia(double x, int n) {
+        double resultado = Math.pow(x, n);
+        return resultado;
+    }
+}
---
+++ Tema2/PotenciaPrueba.java
@@ -0,0 +1,10 @@
+package Tema2;
+
+public class PotenciaPrueba {
+    public static void main(String[] args) {
+        double prueba1 = Potencia.potencia (3.3,5);
+        double prueba2 = Potencia.potencia (2.2,9);
+        System.out.println(prueba1);
+        System.out.println(prueba2);
+    }
+}
