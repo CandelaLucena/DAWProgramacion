e5368fbb9743ad368f5e1d9807ee8b93efaed2b5
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-11-12 10:14:12 +0100

    prueba

--- Tema2/Positivos.java
+++ Tema2/Positivos.java
@@ -1,6 +1,16 @@
 public class Positivos {
+    public static int positivos(int [] numeros){
+        int resultado = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i]>=0){
+                resultado++;
+            }
+        }
+        return resultado;
+    }
     public static void main(String[] args) {
-        int[] numeros = {5, 4, 7, -2, 10, 1};
-        System.out.println (positivos(numeritos));
+        int[] numeros2 = {5, 4, 7, -2, 10, 1};
+        System.out.println (positivos(numeros2));
     }
 }
