fb0f6758e8144ec90d58a1ccc87179f58384a20d
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-11-15 09:43:42 +0100

    Profesor

---
+++ Profesor/Tema1/E2.java
@@ -0,0 +1,13 @@
+class Ejemplo2
+{
+    public static void main(String args[])
+    {
+    	int dividendo = (int) 20.0;
+    	
+    	int divisor = (int) 6.0;
+    	
+    	int sumarAlFinal = (int) 3.0;
+    	
+        System.out.println((dividendo / divisor) + sumarAlFinal);
+    }
+}
