e894be66f25704676121d42b2d1277c6ae06f6d3
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-11-19 10:25:35 +0100

    Ejercicio38 terminado

--- Tema1/Ejercicio38.java
+++ Tema1/Ejercicio38.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Ejercicio38 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-        char[] vocales = { 'a', 'e', 'i', 'o', 'u'};
         int numeroVocales = 0;
         String cadenaTexto;
         System.out.print("Ingrese el texto:");
         cadenaTexto = scanner.nextLine();
         scanner.close();
-
-        
+        for (int i = 0; i < cadenaTexto.length(); i++) {
+            if ((cadenaTexto.charAt(i)== 'a')||(cadenaTexto.charAt(i)== 'e')||(cadenaTexto.charAt(i)== 'i')||(cadenaTexto.charAt(i)== 'o')||(cadenaTexto.charAt(i)== 'u')){
+                numeroVocales++;
+            }
+        }
+        System.out.println("El numero de vocales es: "+numeroVocales);
     }
 }
