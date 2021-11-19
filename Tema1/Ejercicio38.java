03d6e33871ba62bbeebc11e50955655207536b53
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-11-19 10:16:47 +0100

    Ejercicio38

--- Tema1/Ejercicio38.java
+++ Tema1/Ejercicio38.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Ejercicio38 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        char[] vocales = { 'a', 'e', 'i', 'o', 'u'};
+        int numeroVocales = 0;
+        String cadenaTexto;
+        System.out.print("Ingrese el texto:");
+        cadenaTexto = scanner.nextLine();
+        scanner.close();
+
         
     }
 }
