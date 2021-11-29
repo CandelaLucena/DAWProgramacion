e32d0de9d888469ca6d04a1b4654600e4177a09b
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-11-29 09:55:52 +0100

    Duplicado

--- Tema3/Comunes.java
+++ Tema3/Comunes.java
@@ -1,5 +1,10 @@
 public class Comunes {
     public static void main(String[] args) {
+        int[] array1 = {1,2,3,4,5,6,7,8,9,10};
+        int[] array2 = {1,10,11,12,3,16,18,1,9,6};
+    }
+
+    public static void comunes(int[]array1, int[]array2){
         
     }
 }
--- Tema3/Duplicados.java
+++ Tema3/Duplicados.java
@@ -1,5 +1,16 @@
 public class Duplicados {
     public static void main(String[] args) {
-        
+        int[] array = {1, 2, 2, 3, 5, 5, 8, 9, 10, 2};
+        duplicados(array);
+    }
+    public static void duplicados(int[] array){
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                if ( array[i]==array[j]) {
+                    System.out.println(array[i]);
+                    break;
+                }
+            }
+        }
     }
 }
