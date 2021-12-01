928976795756c35913e65267e9afc583ebce06b1
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-12-01 10:09:13 +0100

    Pares Impares terminado

--- Tema3/ParesImpares.java
+++ Tema3/ParesImpares.java
@@ -1,5 +1,22 @@
 public class ParesImpares {
     public static void main(String[] args) {
-        
+        int[] array1 = {1,2,3,4,5,6,7,8,9,10};
+        paresimpares(array1);
+    }
+    public static void paresimpares(int[]array1){
+        int enterosPares = 0;
+        int enterosImpares = 0;
+
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i]%2==0){
+                System.out.println(array1[i]+" es entero par");
+                enterosPares++;
+            }else{
+                System.out.println(array1[i]+" es entero impar");
+                enterosImpares++;
+            }
+        }
+        System.out.println("Hay "+enterosPares+" enteros pares");
+        System.out.println("Hay "+enterosImpares+" enteros impares");
     }
 }
