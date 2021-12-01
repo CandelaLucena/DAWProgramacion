e5bc8e4a3b844336d3c079c03bc1302c9df7c422
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-12-01 08:29:04 +0100

    Comunes terminado

--- Tema3/Comunes.java
+++ Tema3/Comunes.java
@@ -2,9 +2,16 @@ public class Comunes {
     public static void main(String[] args) {
         int[] array1 = {1,2,3,4,5,6,7,8,9,10};
         int[] array2 = {1,10,11,12,3,16,18,1,9,6};
+        comunes(array1, array2);
     }
 
     public static void comunes(int[]array1, int[]array2){
-        
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]){
+                    System.out.println(array1[i]);
+                }
+            }
+        }
     }
 }
