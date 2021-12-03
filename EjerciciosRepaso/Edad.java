0670c60b2436cf90a1c782a292511c24be7dce64
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-12-03 09:36:12 +0100

    Ejercicios Repaso Edad

--- EjerciciosRepaso/Edad.java
+++ EjerciciosRepaso/Edad.java
@@ -1,5 +1,7 @@
-//?
-
+import java.util.Date;
+//import java.time.Month;
+import java.time.ZoneId;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Edad {
@@ -8,14 +10,27 @@ public class Edad {
         int mes;
         int anno;
         int edad;
+    
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese su fecha de nacimiento.");
-        System.out.print("Dia:");
+        System.out.println("Ingrese su fecha de nacimiento.");
+        System.out.print("Dia: ");
         dia = scanner.nextInt();
-        System.out.print("Mes:");
+        System.out.print("Mes: ");
         mes = scanner.nextInt();
-        System.out.print("Anno:");
+        System.out.print("Anno: ");
         anno = scanner.nextInt();
         scanner.close();
+        
+        dia = dia-mes;
+
+        Date date = new Date();
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        int year  = localDate.getYear();
+        //int month = localDate.getMonthValue();
+        //int day   = localDate.getDayOfMonth();
+        
+        edad = year - anno;
+
+        System.out.print("Usted tiene "+edad+" annos");
     }
 }
