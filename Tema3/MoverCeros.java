f61b86549c3b9453ce4f887d92032dc413fb7064
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-12-03 08:36:17 +0100

    Reestructuracion

--- Tema3/MoverCeros.java
+++ Tema3/MoverCeros.java
@@ -1,17 +1,20 @@
 public class MoverCeros {
     public static void main(String[] args) {
         int[] array1 = {1,0,2,3,4,0,0,5,6,0,7,8,9,0,10};
-        moverceros(array1);
+        moverCeros(array1);
 
         for (int i = 0; i < array1.length; i++) {
             System.out.println(array1[i]);
         }
     }
 
-    public static void moverceros(int[]array1){
-        for (int j = array1.length; j < 0; j--) {
+    public static void moverCeros(int[]array1){
+        for (int j = array1.length - 1; j >= 0; j--) {
             if (array1[j]==0){
-                array1[j]=array1[j-1];
+                for (int k = j; k < array1.length-1; k++) {
+                    array1[k]=array1[k+1];
+                    array1[k+1] = 0;
+                }
             }
         }
     }
