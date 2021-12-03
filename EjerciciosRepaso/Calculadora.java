36b8ec6b4c7560a39d86eb37ed012c0c33fad2bc
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-12-03 10:25:39 +0100

    Ejercicios de repaso corregidos

--- EjerciciosRepaso/Calculadora.java
+++ EjerciciosRepaso/Calculadora.java
@@ -1,5 +1,3 @@
-//?
-
 import java.util.Scanner;
 
 public class Calculadora {
@@ -8,28 +6,35 @@ public class Calculadora {
         String operador;
         int operando2;
         int resultado;
+        String mas = "+";
+        String menos = "-";
+        String divisor = "/";
+        String multiplicador = "*";
+        
         Scanner scanner = new Scanner(System.in);
         System.out.print("Operando:");
         operando1 = scanner.nextInt();
+        scanner.nextLine();
         System.out.print("Operando:");
         operador = scanner.nextLine();
         System.out.print("Operando:");
         operando2 = scanner.nextInt();
+        scanner.nextLine();
         scanner.close();
 
-        if(operador == "+"){
+        if(operador.equals(mas)){
             resultado = operando1 + operando2;
             System.out.print(operando1+" + "+operando2+" = "+resultado);
         }
-        else if(operador == "-"){
+        else if(operador.equals(menos)){
             resultado = operando1 - operando2;
             System.out.print(operando1+" - "+operando2+" = "+resultado);
         }
-        else if(operador == "/"){
+        else if(operador.equals(divisor)){
             resultado = operando1 / operando2;
             System.out.print(operando1+" / "+operando2+" = "+resultado);
         }
-        else if(operador == "**"){
+        else if(operador.equals(multiplicador)){
             resultado = operando1 * operando2;
             System.out.print(operando1+" * "+operando2+" = "+resultado);
         }
--- EjerciciosRepaso/Ordenamiento3.java
+++ EjerciciosRepaso/Ordenamiento3.java
@@ -1,16 +1,31 @@
-//?
-
+//import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ordenamiento3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] numeros = new int[4];
-
+        int mayor;
         for (int i = 0; i < numeros.length; i++) {
             System.out.print("Ingrese un número:");
             numeros[i] = scanner.nextInt();
         }
+
         scanner.close();
+
+        //Arrays.sort(numeros);
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 1; j < (numeros.length - i); j++) {
+                if (numeros[j - 1] > numeros[j]) {
+                    mayor = numeros[j - 1];
+                    numeros[j - 1] = numeros[j];
+                    numeros[j] = mayor;
+                }
+            }
+        }
+
+        for (int i = 0; i < numeros.length; i++) {
+             System.out.print(numeros[i]+" ");
+        }
     }
 }
