3f64e476d0df1de9f7148aecbbea394d9fecd1ab
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-12-10 10:42:20 +0100

    Set de tennis casi terminado

--- EjerciciosRepaso/SetDeTenis.java
+++ EjerciciosRepaso/SetDeTenis.java
@@ -1,7 +1,23 @@
-//?
+import java.util.Scanner;
 
 public class SetDeTenis {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int equipoA = 0;
+        int equipoB = 0;
         
+        System.out.print("Juegos ganados por A: ");
+        equipoA = scanner.nextInt();
+        System.out.print("Juegos ganados por B: ");
+        equipoB = scanner.nextInt();
+        scanner.close();
+
+        if((equipoA-equipoB == 2)||(equipoB-equipoA == -2)){
+            System.out.println("Gano A");
+        }else if((equipoB-equipoA == 2)||(equipoA-equipoB == -2)){
+            System.out.println("Gano B");
+        }else if((equipoB-equipoA > 2)||(equipoA-equipoB < -2)||(equipoA-equipoB > 2)||(equipoB-equipoA < -2)){
+            System.out.println("Invalido");
+        }
     }
 }
