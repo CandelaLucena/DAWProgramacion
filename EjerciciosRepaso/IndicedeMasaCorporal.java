26ae039b3f2d4f5387eece03160572447abfd39f
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-12-10 10:15:14 +0100

    Indice de masa corporal y letraonumero terminados

--- EjerciciosRepaso/IndicedeMasaCorporal.java
+++ EjerciciosRepaso/IndicedeMasaCorporal.java
@@ -1,7 +1,25 @@
-//?
+import java.util.Scanner;
 
 public class IndicedeMasaCorporal {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int edad = 0;
+        double IMC = 0.0;
 
+        System.out.print("Ingrese su edad: ");
+        edad = scanner.nextInt();
+        System.out.print("Ingrese su IMC: ");
+        IMC = scanner.nextDouble();
+        scanner.close();
+
+        if((edad<45)&&(IMC<22.0)){
+            System.out.println("Bajo");
+        }else if((edad>=45)&&(IMC<22.0)){
+            System.out.println("Medio");
+        }else if((edad<45)&&(IMC>=22.0)){
+            System.out.println("Medio");
+        }else if((edad>=45)&&(IMC>=22.0)){
+            System.out.println("Alto");
+        }
     }
 }
--- EjerciciosRepaso/LetraONumero.java
+++ EjerciciosRepaso/LetraONumero.java
@@ -1,6 +1,24 @@
-//?
+import java.util.Scanner;
 
 public class LetraONumero {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String letraNumero;
+        
+        System.out.print("Ingrese caracter: ");
+        letraNumero = scanner.next();
+        scanner.close();
+
+        char leerLetraNumero = letraNumero.charAt(0);
+
+        if((leerLetraNumero>='0')&&(leerLetraNumero<='9')){
+            System.out.println("Es un numero");
+        }else if((leerLetraNumero>='a')&&(leerLetraNumero<='z')){
+            System.out.println("Es letra minúscula.");
+        }else if((leerLetraNumero>='A')&&(leerLetraNumero<='Z')){
+            System.out.println("Es letra mayúscula.");
+        }else{
+            System.out.println("No es letra ni número.");
+        }
     }
 }
