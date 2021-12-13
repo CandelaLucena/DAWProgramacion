74cf6e5063345890408f1db43224ff9687c0ba9f
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-12-13 12:12:16 +0100

    Divisores terminado

--- EjerciciosRepasoCiclos/Divisores.java
+++ EjerciciosRepasoCiclos/Divisores.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Divisores {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero = 0;
+        System.out.print("Ingrese numero: ");
+        numero = scanner.nextInt();
+        scanner.close();
+        for (int i = 1; i <= numero; i++) {
+            if(numero%i==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
 }
