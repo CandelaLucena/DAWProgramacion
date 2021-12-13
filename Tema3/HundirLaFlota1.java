2ca54076e002b1e71d287fa9373a61b555a6f454
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-12-13 09:15:50 +0100

    Comienzo de hundir la flota

--- Tema3/HundirLaFlota1.java
+++ Tema3/HundirLaFlota1.java
@@ -1,3 +1,21 @@
 public class HundirLaFlota1 {
+    public static void createShips(int numShips, int[][] shipPlaces) {
+        int r1, r2;
+        int cont = 0;
     
+        while (cont < numShips) {
+            r1 = (int) (Math.random()*8); 
+            r2 = (int) (Math.random()*8);
+            if (shipPlaces[r1][r2] != 1) {
+                shipPlaces[r1][r2]=1;
+                cont++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[][] shipPlaces=new int[8][8];
+        createShips(10, shipPlaces);
+
+        
+    }
 }
