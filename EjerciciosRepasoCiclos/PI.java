679e6fcd665b508d3fe28c28cf3be6b2348b59b7
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-12-15 10:00:24 +0100

    PI

--- EjerciciosRepasoCiclos/PI.java
+++ EjerciciosRepasoCiclos/PI.java
@@ -1,3 +1,27 @@
+//?
+import java.util.Scanner;
+
 public class PI {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero = 0;
+        int contador = 0;
+
+        double PI = 0;
+        double resultado = 0;
+        
+        System.out.print("n: ");
+        numero = scanner.nextInt();
+        scanner.close();
+
+        while (contador==numero) {
+            if (!(contador%2==0)){
+                resultado = resultado + 1/contador;
+            }
+            contador++;
+        }
+
+        PI = 4 * (1 - resultado);
+        System.out.println(PI);
+    }
 }
