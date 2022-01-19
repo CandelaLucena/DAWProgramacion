3acb9fedf6e9657d265547f4f745e20112fbfcd3
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-01-19 10:35:50 +0100

    SillaTest y Enunciado modificados

--- Tema5/SillaTest.java
+++ Tema5/SillaTest.java
@@ -5,8 +5,9 @@ public class SillaTest {
 
         // s1.comodidad = 4; No se puede acceder porque esta privado
         System.out.println(s1.getComodidad());
-
         s1.setComodidad(4);
         s1.setNumPatas(2000);
+
+        
     }
 }
