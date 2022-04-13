59785d9f6040e92b23fb54b709f47434eb61cf3c
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-04-13 10:38:43 +0200

    contar palabras

--- Tema7/Ejercicio5ContarPalabrFiles.java
+++ Tema7/Ejercicio5ContarPalabrFiles.java
@@ -1,17 +1,24 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ejercicio5ContarPalabrFiles {
+
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner1 = new Scanner(new File("DAWProgramacion/Tema7/fichero4.txt"));
-        ArrayList<String> leerfichero1 = new ArrayList<String>();
-        while (scanner1.hasNext()){
-            leerfichero1.add(scanner1.next());
+        String patron = "";
+        int contador = 0;
+
+        Scanner scanner2 = new Scanner(System.in);
+        patron = scanner2.nextLine();
+        scanner2.close();
+
+        while (scanner1.hasNext(patron)){
+            contador++;
         }
         scanner1.close();
-
+        System.out.println(contador);
+        
 
         
     }
