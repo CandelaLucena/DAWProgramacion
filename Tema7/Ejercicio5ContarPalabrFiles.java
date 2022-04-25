68b5fe4f934ac7437c7403e36926ff465fdea88f
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-04-25 09:52:56 +0200

    Ejercicio 5

--- Tema7/Ejercicio5ContarPalabrFiles.java
+++ Tema7/Ejercicio5ContarPalabrFiles.java
@@ -8,13 +8,17 @@ public class Ejercicio5ContarPalabrFiles {
         Scanner scanner1 = new Scanner(new File("DAWProgramacion/Tema7/fichero4.txt"));
         String patron = "";
         int contador = 0;
-
+        System.out.println("Escriba el patron");
         Scanner scanner2 = new Scanner(System.in);
         patron = scanner2.nextLine();
         scanner2.close();
 
-        while (scanner1.hasNext(patron)){
-            contador++;
+        while (scanner1.hasNext()){
+            if(scanner1.hasNext(patron)){
+                contador++;
+            }else{
+
+            }
         }
         scanner1.close();
         System.out.println(contador);
