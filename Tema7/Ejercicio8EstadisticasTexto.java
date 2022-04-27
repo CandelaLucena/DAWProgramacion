2df9966e8e36d9824a4e5c10a29a5de55323a68c
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-04-27 10:32:37 +0200

    mitad del ejercicio 8

--- Tema7/Ejercicio8EstadisticasTexto.java
+++ Tema7/Ejercicio8EstadisticasTexto.java
@@ -7,16 +7,40 @@ public class Ejercicio8EstadisticasTexto {
         Scanner scanner1 = new Scanner(new File("DAWProgramacion/Tema7/fichero5.txt"));
         int contadorLetras = 0;
         int contadorPalabras = 0;
-        int contadorLineas = 0;
+        int contadorLineas = 1;
         int contadorVocales = 0;
         int contadorConsonantes = 0;
+        int contarLetraPalabra = 0;
+        int contadorEspacios = 0;
+        int contadorTabulacion = 0;
 
         do{
-            if(scanner1.hasNext() == true){
-                scanner1.next();
-            }else{
-                scanner1.next();
+            String palabra = scanner1.next();
+            contarLetraPalabra = (int) palabra.chars().count();
+            for (int index = 0; index < palabra.length(); index++) {
+                char letra= palabra.charAt(index);
+                if(letra == 'a'|| letra == 'e'|| letra == 'i' ||letra == 'o' ||letra == 'u'||letra == 'á' ||letra == 'é' ||letra == 'í' 
+                 ||letra == 'ó' ||letra == 'ú'){
+                    contadorVocales++;
+                    
+                }else{
+                    contadorConsonantes++;
+                }
             }
+            contadorLetras = contarLetraPalabra + contadorLetras;
+            contadorPalabras++;
         }while (scanner1.hasNext());
+
+        do{
+            contadorLineas++;
+        }while(scanner1.hasNextLine());
+
+        System.out.println("Letras: "+contadorLetras);
+        System.out.println("Palabras: "+contadorPalabras);
+        System.out.println("Lineas: "+contadorLineas);
+        System.out.println("Vocales: "+contadorVocales);
+        System.out.println("Consonantes: "+contadorConsonantes);
+        System.out.println("Espacios: "+contadorEspacios);
+        System.out.println("Tabulaciones: "+contadorTabulacion);
     }
 }
