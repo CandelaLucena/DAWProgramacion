9bc437beb4a106534e37d77e42371015f665715c
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-05-11 09:23:22 +0200

    XML Generar y Leer completado

--- Control04SerializableXMLJSON/CrearXML.java
+++ Control04SerializableXMLJSON/CrearXML.java
@@ -1,6 +1,5 @@
 import java.io.EOFException;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -9,37 +8,55 @@ import javax.xml.crypto.dsig.TransformException;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
 
 public class CrearXML {
     public static void main(String[] args) 
-        throws IOException, ClassNotFoundException, ParserConfigurationException, TransformException {
+        throws IOException, ClassNotFoundException, ParserConfigurationException, TransformException, TransformerFactoryConfigurationError, TransformerException {
     
-        ObjectInputStream file = new ObjectInputStream(new FileInputStream("DAWProgramacion/Control04SerializableXMLJSON/clientes.xml"));
+        ObjectInputStream file = new ObjectInputStream(new FileInputStream("DAWProgramacion/Control04SerializableXMLJSON/clientes.dat"));
         
         Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-        Element root = doc.createElement("clientes");
-        doc.appendChild(root);
+        Element clientes = doc.createElement("clientes");
+        doc.appendChild(clientes);
 
         Cliente e;
         try{
             while(true){
                 e = (Cliente) file.readObject();
 
-                Element emp = doc.createElement("cliente");
-                emp.setAttribute("numero", String.toString(e.getId()));
+                Element cliente = doc.createElement("cliente");
+                //emp.setAttribute("numero", String.substring(e.getId())); ATRIBUTO
+                clientes.appendChild(cliente);
+                
+                Element hijo = doc.createElement("id");
+                hijo.appendChild(doc.createTextNode(e.getId()));
+                cliente.appendChild(hijo);
 
-                Element hijo = doc.createElement("nombre");
-                hijo.appendChild(doc.createTextNode(e.getNombre()));
-                emp.appendChild(hijo);
+                Element hijo1 = doc.createElement("nif");
+                hijo1.appendChild(doc.createTextNode(e.getNif()));
+                cliente.appendChild(hijo1);
 
+                Element hijo2 = doc.createElement("nombre");
+                hijo2.appendChild(doc.createTextNode(e.getNombre()));
+                cliente.appendChild(hijo2);
 
+                Element hijo3 = doc.createElement("apellido");
+                hijo3.appendChild(doc.createTextNode(e.getApellidos()));
+                cliente.appendChild(hijo3);
+
+                Element hijo4 = doc.createElement("email");
+                hijo4.appendChild(doc.createTextNode(e.getEmail()));
+                cliente.appendChild(hijo4);
+
+                
             }
         } catch (EOFException eof) {
             file.close();
