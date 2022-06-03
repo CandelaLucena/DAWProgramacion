038ff69f6ea55142426c410c5e2870b0ae139bed
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-06-03 08:58:34 +0200

    Base de datos

--- Tema8/TestDB.java
+++ Tema8/TestDB.java
@@ -11,10 +11,10 @@ public class TestDB {
 
     public static void main(String[] args) {
         String sqlSelectAllPersons = "SELECT count(*) FROM Cliente";
-        String connectionUrl = "jdbc:mysql://192.168.204.171:3306/test?serverTimezone=UTC";
+        String connectionUrl = "jdbc:mysql://192.168.204.168:3306/Clientes";
 
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
@@ -25,11 +25,12 @@ public class TestDB {
                 ResultSet rs = ps.executeQuery()) {
 
             while (rs.next()) {
-                long id = rs.getLong("ID");
-                String name = rs.getString("FIRST_NAME");
-                String lastName = rs.getString("LAST_NAME");
+                long id = rs.getLong("id");
+                String name = rs.getString("nombre");
+                String lastName = rs.getString("apellido");
 
                 // do something with the extracted data...
+                System.out.println(id+" "+name+" "+lastName);
             }
         } catch (SQLException e) {
             e.printStackTrace();
